98d3bb1412f0bca603b91732f8ff3a3788c04337
shinilseop <shinilseop12@gmail.com>
2022-07-11 00:31:16 +0900

    chore: init petclinic springboot2 project

--- spring_security_jwt_simple/src/main/java/com/example/spring_security_jwt_simple/security/SecurityController.java
+++ spring_security_jwt_simple/src/main/java/com/example/spring_security_jwt_simple/security/SecurityController.java
@@ -17,9 +17,8 @@ public class SecurityController {
 
   @GetMapping("/create/token")
   public Map<String, Object> createToken(@RequestParam(value = "subject") String subject) {
-    System.out.println("get Request!");
+    // 받은 subject를 기반으로 2분간 유지되는 token을 하나 만들어서 반환
     String token = securityService.createToken(subject, (2 * 1000 * 60));
-    System.out.println("token: "+token);
     Map<String, Object> map = new LinkedHashMap<>();
     map.put("result", token);
     return map;
@@ -27,6 +26,7 @@ public class SecurityController {
 
   @GetMapping("/get/subject")
   public Map<String, Object> getSubject(@RequestParam(value = "token") String token) {
+    // 받은 token을 기반으로 그에 맞는 subject를 찾아서 반환
     String subject = securityService.getSubject(token);
     Map<String, Object> map = new LinkedHashMap<>();
     map.put("result", subject);
--- spring_security_jwt_simple/src/main/java/com/example/spring_security_jwt_simple/security/SecurityService.java
+++ spring_security_jwt_simple/src/main/java/com/example/spring_security_jwt_simple/security/SecurityService.java
@@ -16,16 +16,17 @@ public class SecurityService {
   private static final String SECRET_KEY = "lfhfwafhbwafbakjfnwlkfnlbefihbwaejkfhbjjdhvwhjd";
 
   public String createToken(String subject, long expTime) {
-    if (expTime <= 0) {
+    if (expTime <= 0) { // expTime 시간 체크
       throw new RuntimeException("만료시간이 0보다 커야됩니다.");
     }
 
-    SignatureAlgorithm signatureAlgorithm = SignatureAlgorithm.HS256;
+    SignatureAlgorithm signatureAlgorithm = SignatureAlgorithm.HS256; // 서명 암호화 알고리즘 선택
 
-    byte[] secretKeyBytes = DatatypeConverter.parseBase64Binary(SECRET_KEY);
-    System.out.println(secretKeyBytes);
-    Key signingKey = new SecretKeySpec(secretKeyBytes, signatureAlgorithm.getJcaName());
+    byte[] secretKeyBytes = DatatypeConverter.parseBase64Binary(SECRET_KEY); // 시크릿키를 base64binary로
 
+    Key signingKey = new SecretKeySpec(secretKeyBytes, signatureAlgorithm.getJcaName()); // 바이트화한 시크릿 키를 기반으로 키생성
+
+    // 생성한 키를 바탕으로 Jwts 생성
     return Jwts.builder()
         .setSubject(subject)
         .signWith(signingKey, signatureAlgorithm)
@@ -35,6 +36,7 @@ public class SecurityService {
 
 
   public String getSubject(String token) {
+    // 입력받은 token을 기반으로 파싱해서 subject 찾기
     Claims claims = Jwts.parserBuilder()
         .setSigningKey(DatatypeConverter.parseBase64Binary(SECRET_KEY))
         .build()
