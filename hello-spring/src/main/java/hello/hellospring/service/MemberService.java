ae7c07504c53d8e56c9cb48a25d021a6341f3016
shinilseop <shinilseop12@gmail.com>
2022-07-11 21:02:45 +0900

    chore: MemberService, MemberRepository bean regist way change to use configure annotation

--- hello-spring/src/main/java/hello/hellospring/service/MemberService.java
+++ hello-spring/src/main/java/hello/hellospring/service/MemberService.java
@@ -5,11 +5,14 @@ import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.repository.MemoryMemberRepository;
 import java.util.List;
 import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 public class MemberService {
 
   private final MemberRepository memberRepository;
 
+  @Autowired
   public MemberService(MemberRepository memberRepository) {
     this.memberRepository = memberRepository;
   }
